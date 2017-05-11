01fed1b3eba0e084c279ac9a31d7f188bd16a7e9
Konstantin Kolesov <Konstantin_Kolesov@epam.com>
2017-05-12 00:14:48 +0300

    test GPIO invocation is added

--- src/main/java/my/garage/gates/service/GateServiceGpio.java
+++ src/main/java/my/garage/gates/service/GateServiceGpio.java
@@ -1,13 +1,23 @@
 package my.garage.gates.service;
 
+import my.garage.gates.experiments.ControlGpioExample;
 import my.garage.gates.model.GateEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GateServiceGpio implements GateServices{
+    @Autowired
+    ControlGpioExample controlGpioExample;
 
     @Override
     public GateEntity open() {
+        try {
+            controlGpioExample.ledTest();
+        } catch (InterruptedException e) {
+            System.err.println("OOOPS");
+            e.printStackTrace();
+        }
         return null;
     }
 
