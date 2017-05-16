8710ada7d7031158ee66406180b5bd9238e58842
Konstantin Kolesov <Konstantin_Kolesov@epam.com>
2017-05-16 23:22:03 +0300

    method definitions, statci content added to resources

--- public/angular/controllers/MainController.js
+++ public/angular/controllers/MainController.js
@@ -1,6 +1,43 @@
 class MainController {
 	constructor($scope, GateService, $state) {
 
+		console.log("in main ctrl");
+
+		$scope.testObj = {wtf: "ok"};
+
+// 		full opening/closing
+		$scope.fullOpen = () => {
+			console.log("open");
+		};
+		$scope.fullClose = () => {
+			console.log("close");
+		};
+
+		$scope.isMoving = false;
+
+// 		movenment up
+		$scope.startMovingUp = () => {
+			console.log("moving up");
+			$scope.isMoving = true;
+		};
+		$scope.stopMovingUp = () => {
+			if ($scope.isMoving){
+				console.log("stop moving up");
+				$scope.isMoving = false;
+			}
+		};
+
+// 		movenment down
+		$scope.startMovingDown = () => {
+			console.log("start moving down");
+			$scope.isMoving = true;
+		};
+		$scope.stopMovingDown = () => {
+			if ($scope.isMoving) {
+				console.log("stop moving down");
+				$scope.isMoving = false;
+			}
+		};
 	}
 }
 
